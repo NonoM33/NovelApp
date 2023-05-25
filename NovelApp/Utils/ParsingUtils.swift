c3e5fb754b433fe642880a43cb73fa527e982580
Renaud cosson <renaudlemagicien@gmail.com>
2023-05-25 18:55:59 +0200

    Add ParsingUtils.swift with replaceUnderscoreWithSpace and formatDate functions
    This commit adds a new file, ParsingUtils.swift, which contains two static functions: replaceUnderscoreWithSpace and formatDate. The former replaces underscores in a string with spaces, while the latter formats a date string into a more readable format.

---
+++ NovelApp/Utils/ParsingUtils.swift
@@ -0,0 +1,29 @@
+//
+//  ParsingUtils.swift
+//  NovelApp
+//
+//  Created by renaud on 25/05/2023.
+//
+
+import Foundation
+
+class Utils {
+    static func replaceUnderscoreWithSpace(in string: String) -> String {
+        return string.replacingOccurrences(of: "_", with: " ")
+    }
+    static func formatDate(_ dateStr: String) -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        
+        guard let date = dateFormatter.date(from: dateStr) else {
+            return nil
+        }
+        
+        dateFormatter.dateFormat = "dd/MM/yy - HH:mm"
+        let formattedDate = dateFormatter.string(from: date)
+        
+        return formattedDate
+    }
+
+}
