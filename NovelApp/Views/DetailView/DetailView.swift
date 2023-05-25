d987764c5b669465fa6d15a23b182dee41fe07e9
Renaud cosson <renaudlemagicien@gmail.com>
2023-05-25 20:32:57 +0200

    Wip: Add performers to DetailView
    This commit adds the ability to display performers in the DetailView. It also includes a new function that sets up performer cards and a new class, DetailViewModel, which is responsible for handling events.

--- NovelApp/Views/DetailView/DetailView.swift
+++ NovelApp/Views/DetailView/DetailView.swift
@@ -6,15 +6,53 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct DetailView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @ObservedObject var model: DetailViewModel
+    @State private var selectedPerformert: Event.Performers?
 
-struct DetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailView()
+    init(event: Event) {
+        self.model =  DetailViewModel(event: event)
     }
+
+    var body: some View {
+        VStack {
+            ScrollView {
+                ForEach(model.event.performers ?? []) { performer in
+                    VStack {
+                        setupPerformerCard(performer)
+                    }
+                }
+            }.navigationTitle("Performers")
+        }
+    }
+
+    @ViewBuilder
+    func setupPerformerCard(_ performer: Event.Performers) -> some View {
+        let typePerfomer = Utils.replaceUnderscoreWithSpace(in: performer.type ?? "")
+        Link(destination: URL(string: performer.url ?? BASE_URL)!) {
+            ZStack {
+                KFImage(performer.image?.toURL())
+                    .resizable()
+                    .overlay {
+                        Rectangle()
+                            .foregroundColor(.R.Primary)
+                            .opacity(0.8)
+                    }
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: .infinity)
+                    .cornerRadius(12)
+                VStack {
+                    Text(performer.name ?? "")
+                        .foregroundColor(.R.SurfaceLight)
+                        .font(.R.section1)
+                    Text(typePerfomer)
+                        .foregroundColor(.R.Star)
+                        .font(.R.section2)
+                }
+            }.padding()
+        }
+    }
+
 }
--- NovelApp/Views/DetailView/DetailViewModel.swift
+++ NovelApp/Views/DetailView/DetailViewModel.swift
@@ -7,14 +7,10 @@
 
 import SwiftUI
 
-struct DetailViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+class DetailViewModel: ObservableObject {
+    @Published var event: Event
 
-struct DetailViewModel_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailViewModel()
+    init(event: Event) {
+        self.event = event
     }
 }
