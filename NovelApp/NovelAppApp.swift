13331b3fe32c14368121c59538d9705327c4540c
Renaud cosson <renaudlemagicien@gmail.com>
2023-05-25 11:24:28 +0200

    Initial Commit

---
+++ NovelApp/NovelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovelAppApp.swift
+//  NovelApp
+//
+//  Created by renaud on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct NovelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
