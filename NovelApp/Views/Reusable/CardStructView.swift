77cb70cd79fc9ba4c7daa2093f44ae200e5bbbdb
Renaud cosson <renaudlemagicien@gmail.com>
2023-05-25 17:36:57 +0200

    Wip: Add new files and extensions for Colors, Fonts, Metrics and CardStructView - Added new files for events.json, eventsDetail.json, Colors.swift, Fonts.swift, Metrics.swift and CardStructView.swift - Created extensions for Color, UIColor and Font with various sizes and weights - Defined constants for animation duration and various CGFloat values

---
+++ NovelApp/Views/Reusable/CardStructView.swift
@@ -0,0 +1,20 @@
+//
+//  CardStructView.swift
+//  NovelApp
+//
+//  Created by renaud on 25/05/2023.
+//
+
+import SwiftUI
+
+struct CardStructView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardStructView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardStructView()
+    }
+}
