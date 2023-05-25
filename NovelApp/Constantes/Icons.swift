825ed52d9aaedc0ddc1f217ec3eb2a6731ed421d
Renaud cosson <renaudlemagicien@gmail.com>
2023-05-25 18:57:13 +0200

    wip: add json

---
+++ NovelApp/Constantes/Icons.swift
@@ -0,0 +1,12 @@
+//
+//  Icons.swift
+//  NovelApp
+//
+//  Created by renaud on 25/05/2023.
+// swiftlint:disable identifier_name
+
+import Foundation
+
+// MARK: SF Synmbol
+
+let ICON_QR_CODE = "qrcode.viewfinder"
