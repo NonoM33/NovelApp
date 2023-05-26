7d7c3bad7dd40608bcf48c0073c68deaeb4cc5cd
Renaud cosson <renaudlemagicien@gmail.com>
2023-05-26 04:55:23 +0200

    Add toString() method to SortOption enum
    This commit adds a new method called toString() to the SortOption enum. This method returns a string representation of each case in the enum.

--- NovelApp/enums/SortOption.swift
+++ NovelApp/enums/SortOption.swift
@@ -7,9 +7,22 @@
 
 import Foundation
 
-enum SortOption {
+enum SortOption: String {
     case `default`
     case byPriceLowToHigh
     case byPriceHighToLow
     case byDate
+
+    func toString() -> String {
+        switch self {
+        case .default:
+            return EMPTY_TXT
+        case .byPriceLowToHigh:
+            return SORT_PRICE_LOW_TO_HIGH
+        case .byPriceHighToLow:
+            return SORT_PRICE_HIGH_TO_LOW
+        case .byDate:
+            return SORT_DATE
+        }
+    }
 }
